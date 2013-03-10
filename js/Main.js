78b02a58896a7d08f4072913ede9f07e41d11c01
Ed Salvana <esalvana@esalvana-mac-nb.gateway.2wire.net>
2013-03-10 15:58:28 -0700

    Building rest of the page

--- js/Main.js
+++ js/Main.js
@@ -1,12 +1,16 @@
 var img;
 var heroHeight;
+var audienceNav;
 
 $(document).ready(function() {
 
     //get a reference to the image inside the hero and the height of the hero
-    img        = $("#wat");
-    heroHeight = parseInt( $("#content").css("margin-top") );
-    //$(window).scroll( onScroll );
+    img        = $("#hero");
+    heroHeight = 530; //parseInt( $("#content").css("margin-top") );
+
+    audienceNav = $("#audienceNavigation"); 
+
+    $(window).scroll( onScroll );
 
 });
 
@@ -14,4 +18,14 @@ function onScroll( e ){
     //calculate the target opacity
     var targetOpacity = Math.max( ( heroHeight - $(window).scrollTop() ) / heroHeight, 0 );
     img.css("opacity", targetOpacity );
+
+    var y = $(window).scrollTop();
+
+    if(y < 530 ){
+    	//$(audienceNav).css("position", "fixed" );
+    	$(audienceNav).css("top", 0);
+    } else {
+    	$(audienceNav).css("top", 530-y);
+		//$(audienceNav).css("position", "relative" );
+    }
 }
